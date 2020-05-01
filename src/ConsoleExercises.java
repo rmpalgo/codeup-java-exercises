4b0c92250d47b1c4aa5f9c228f471704c34b89fd
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-01 12:25:35 -0500

    Scanned for three words.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -11,6 +11,15 @@ public class ConsoleExercises {
         int wholeNumber = scanner.nextInt();
         System.out.printf("The value of number being enter in the terminal is: %d%n", wholeNumber);
 
+        Scanner scanThreeWords = new Scanner(System.in);
+        System.out.print("Line 1: ");
+        String word1 = scanThreeWords.nextLine();
+        System.out.print("Line 2: ");
+        String word2 = scanThreeWords.nextLine();
+        System.out.print("Line 3: ");
+        String word3 = scanThreeWords.nextLine();
+
+        System.out.printf("%s%n%s%n%s%n", word1, word2, word3);
 
 
 
