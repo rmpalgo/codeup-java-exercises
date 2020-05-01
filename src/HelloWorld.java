51c007ea361ca0e73d31bd256348bc2f554a6d40
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-01 10:09:43 -0500

    Finished exercise 13.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -31,11 +31,33 @@ public class HelloWorld {
 
         /* x++ is executing code or adding the 1, and then logging the x again will show the added 1 resulting in x = 6; */
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
         // this assigns the +1 and added it immediately
 
+//        String class = 'Hello there!';
+        //creating variable named class causes an error because class is a reserved keyword
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////      int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x++;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+    int x = 10;
+    int y = 2;
+//    x /= y;
+//    System.out.println( x );
+ y -= x;
+ System.out.println(y);
     }
 
 }
