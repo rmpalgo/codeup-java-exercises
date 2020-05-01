a0d9d1afad758c1da8c947ed8e144630d0b0d130
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-01 12:15:14 -0500

    Utilized scanner Class and prompting an int number in the terminal.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
 
     public static void main(String args[]) {
         double pi = 3.14159;
-        String newString = "Money";
-        System.out.printf("The value of pi is approximately %,.2f.", pi);
+        System.out.printf("The value of pi is approximately %,.2f.%n", pi);
+        System.out.println("Enter a whole number.");
+        Scanner scanner = new Scanner(System.in);
+
+        int wholeNumber = scanner.nextInt();
+        System.out.printf("The value of number being enter in the terminal is: %d%n", wholeNumber);
+
+
+
 
 
     }
