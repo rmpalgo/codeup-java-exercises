0a65e91ef3ad959598ef24c77915beefd31de851
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-01 12:04:50 -0500

    Creatd a string and used pi variable.

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,11 @@
+public class ConsoleExercises {
+
+    public static void main(String args[]) {
+        double pi = 3.14159;
+        String newString = "Money";
+        System.out.printf("The value of pi is approximately %,.2f.", pi);
+
+
+    }
+
+}
