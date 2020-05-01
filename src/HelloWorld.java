eacb5777f47771f0d2768854d3af7f39c64261dd
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-01 09:40:37 -0500

    Exercise up to step 8.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,25 @@ public class HelloWorld {
     public static void main (String[] args) {
         System.out.print("Hello, World!\n");
         System.out.print("Hello, World!\n");
+
+// Step 1: create an int variable and assign favorite number
+        int myFavoriteNumber = 1000000;
+        System.out.println(myFavoriteNumber);
+//  Step 2: create a string variable named myString
+        String myString = "This is a new string Java style.";
+        System.out.println(myString);
+//  Step 3: Change your code to assign the value 3.14159 to myString
+        myString = "New Stuff";
+        System.out.println(myString);
+//  Step 4: Change your code to assign the value 3.14159 to myString
+       /* myString = 3.1459;  This produces an error*/
+
+//  Step 5: Declare a long variable named myNumber, but do not assign anything to it. Next print out myNumber.
+         long myNumber = 123L;
+        System.out.println(myNumber);
+        // Causes an error, variable myNumber might not be initialized.
     }
 
+    // Step 6, 7, 8: Change you code to assign the value 3.14 to myNumber.
+        /* sends and error stating incompatible types, 3.14 is non-integer value and requires double for initializing type */
 }
