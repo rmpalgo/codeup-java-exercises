d8b1fb3c2277aceeaf936770406a0ae319603818
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-04 11:20:53 -0500

    Completed Exercise 1.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,12 @@
+public class ControlFlowExercises {
+
+    public static void main(String args[]) {
+        //While
+        int i = 5;
+        while( i <= 15 ) {
+            System.out.printf("While loop iteration index: %s%n", i);
+            i++;
+        }
+    }
+
+}
