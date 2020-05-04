542c59c40965f5b1528cdfad9ffac7a08c855d46
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-04 11:27:32 -0500

    Completed do while loop for exercise b counting by 2's.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,21 @@ public class ControlFlowExercises {
 
     public static void main(String args[]) {
         //While
+        /*
         int i = 5;
         while( i <= 15 ) {
             System.out.printf("While loop iteration index: %s%n", i);
             i++;
         }
+        */
+
+        //Do While
+        int counter = 0;
+        do {
+            System.out.printf("Counting %s%n", counter);
+            counter +=2;
+        } while(counter <= 100);
+
     }
 
 }
