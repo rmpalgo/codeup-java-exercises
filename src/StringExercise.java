f7abc05d6f39c5ba25287b7d4ea57ba993f0480b
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-05 10:18:56 -0500

    Completed string basics.

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,10 +1,29 @@
 public class StringExercise {
 
     public static void main(String args[]) {
+        /*
         String stringWord = "We don't need no education";
         String newStringWord = stringWord.replace("education", "thought control");
         System.out.println(stringWord);
         System.out.println(newStringWord);
+         */
+
+        /*
+        String word = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(word);
+         */
+
+        /*
+        String word = "In windows, the main drive is usually C:\\";
+        System.out.println(word);
+         */
+
+        String word = "I can do backlashes \\, double backlashes \\\\, \n and the amazing triple backlashes \\\\\\";
+        System.out.println(word);
+
+
+
+
 
 
     }
