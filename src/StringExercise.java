ca5c3ca99e759f09fa297f5d671e846b9b8aa319
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-05 10:13:39 -0500

    Completed part 1 of string basics.

---
+++ src/StringExercise.java
@@ -0,0 +1,12 @@
+public class StringExercise {
+
+    public static void main(String args[]) {
+        String stringWord = "We don't need no education";
+        String newStringWord = stringWord.replace("education", "thought control");
+        System.out.println(stringWord);
+        System.out.println(newStringWord);
+
+
+    }
+
+}
