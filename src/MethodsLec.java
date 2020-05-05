ac851668a99a856b19467ab8a865d734e0a79cf8
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-05 12:07:53 -0500

    First java method.

---
+++ src/MethodsLec.java
@@ -0,0 +1,14 @@
+public class MethodsLec {
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello() {
+        return "Hello Ron";
+    }
+
+
+
+
+}
