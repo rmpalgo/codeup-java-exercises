0d669b13188148a0e98968ed1b5466e3c0b53063
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-05 15:51:26 -0500

    Used for loop for multiply.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -6,6 +6,8 @@ public class MethodsExercises {
         System.out.println("subtract(10, 5) = " + subtract(10, 5));
         System.out.println("multiply(2, 2) = " + multiply(2, 2));
         System.out.println("divide(10, 5) = " + divide(10, 5));
+        System.out.println("modulus(10, 2) = " + modulus(10, 2));
+        System.out.println("multiplyLoop(10, 5) = " + multiplyLoop(10, 5));
 
     }
 
@@ -28,6 +30,16 @@ public class MethodsExercises {
     public static long modulus(long num1, long num2) {
         return num1 % num2;
     }
+
+    public static long multiplyLoop(long num1, long num2) {
+        long result = 0;
+        for(long index = 0; index < num2; index++ ) {
+            result += num1;
+        }
+        return result;
+    }
+
+
 
 
 
