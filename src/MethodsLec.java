d626bfea5967d873dad31e5db4ac761870954bd7
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-05 12:28:53 -0500

    Feat: completed different examples of methods.

--- src/MethodsLec.java
+++ src/MethodsLec.java
@@ -1,11 +1,33 @@
 public class MethodsLec {
 
     public static void main(String[] args) {
-        System.out.println(sayHello());
+        System.out.println("shout('hello milo') = " + shout("hello milo"));
+        System.out.println("division(2, 2) = " + division(2, 2));
+        System.out.println("sum(5, 3) = " + sum(5, 3));
+        System.out.println("sayHello() = " + sayHello("RON"));
+
     }
 
+    /*
     public static String sayHello() {
         return "Hello Ron";
+    }
+     */
+
+    public static String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int division(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static String shout(String word ) {
+        return word.toUpperCase();
     }
 
 
