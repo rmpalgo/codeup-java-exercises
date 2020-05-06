63aaf510dc8aa7baecb77e92a2aef628b0d7ff32
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-06 11:12:24 -0500

    Added max turns and remove 1 per turn.

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class HighLow {
 
     public static void main(String args[]) {
+        int guesses = 10;
         int randomNumber = new java.util.Random().nextInt(100) + 1;
         System.out.println("randomNumber = " + randomNumber);
-        highLow(randomNumber);
+        highLow(randomNumber, guesses);
     }
 
-    public static void highLow(int randomNumber) {
+    public static void highLow(int randomNumber, int guesses) {
+        System.out.println("You have " + guesses + " guesses left");
+        guesses = guesses - 1;
+        if(guesses < 1) {
+            System.out.println("Game over.");
+            return;
+        }
         Scanner myScanner = new Scanner(System.in);
         int input;
         System.out.println("Guess a number between 1 and 100.");
@@ -16,10 +23,10 @@ public class HighLow {
         myScanner.nextLine();
         if(input > randomNumber) {
             System.out.println("LOWER");
-            highLow(randomNumber);
+            highLow(randomNumber, guesses);
         } else if(input < randomNumber) {
             System.out.println("HIGHER");
-            highLow(randomNumber);
+            highLow(randomNumber, guesses);
         } else if(input == randomNumber) {
             System.out.println("GOOD GUESS");
         }
