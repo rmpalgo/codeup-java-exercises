37c69a56715b6c1d5fb3cbdf38c66866237f0115
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-07 15:58:32 -0500

    Tested for getString.

---
+++ src/util/InputTest.java
@@ -0,0 +1,10 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        Input inputTest = new Input();
+        String wordTest = inputTest.getString();
+        System.out.println("wordTest = " + wordTest);
+    }
+}
