d424751fa6a6d59cf1d196d423bc043d2a970da7
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-08 16:25:11 -0500

    Created addPerson method but return array with adresses each element.

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,7 +5,6 @@ public class ArraysExercises {
     public static void main(String[] args) {
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers));
-
         Person[] personArray = new Person[3];
         Person bruce = new Person("Bruce");
         Person jackie = new Person("Jackie");
@@ -18,7 +17,23 @@ public class ArraysExercises {
             System.out.println(person.getName());
         }
 
+        System.out.println("addPerson(people, \"Mia\") = " + addPerson(people, "Mia"));
 
+    }
+    private static Person person1 = new Person("ron");
+    private static Person person2 = new Person("jackie");
+    private static Person person3 = new Person("milo");
+
+   private static Person[] people = {person1, person2, person3};
+
+
+
+
+    public static  String addPerson( Person [] people, String person ) {
+        int newLength = people.length;
+        Person[] addedArray = Arrays.copyOf(people, newLength + 1);
+        addedArray[newLength] = new Person (person);
+        return Arrays.toString(addedArray);
     }
 
 }
