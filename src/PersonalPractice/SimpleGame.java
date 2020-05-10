1fbad8731d268d455159d43b1a82b5dcd99d3a5a
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-10 17:32:20 -0500

    Simple logic to win the game if hits > 3.

--- src/PersonalPractice/SimpleGame.java
+++ src/PersonalPractice/SimpleGame.java
@@ -1,16 +1,32 @@
 package PersonalPractice;
 
+import java.util.Scanner;
+
 public class SimpleGame {
 
     int [] locationCells;
     int numOfHits = 0;
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SimpleGame game = new SimpleGame();
+        int[] locations = {2,3,4};
+        game.setLocationCells(locations);
+
+        while(game.getNumOfHits() < 3) {
+            System.out.println("Guess location: ");
+            String userGuess = input.nextLine();
+            String result = game.checkYourself(userGuess);
+        }
 
     }
 
     public void setLocationCells(int [] locs) {
         this.locationCells = locs;
+    }
+
+    public int getNumOfHits() {
+        return this.numOfHits;
     }
 
     public String checkYourself(String stringGuess) {
