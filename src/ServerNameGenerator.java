6075a6555cd8b36d1ed3c96b978201c64d743813
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-10 11:56:15 -0500

    Exercise 3 randomWord() created and executable.

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
 
 public class ServerNameGenerator {
-    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Server Name: ");
         System.out.println(randomWord());
 
 
     }
+
+
 
     private static String[] stringAdj = {"cool", "bellicose", "calamitous", "sagacious", "tenacious", "omnipotent", "factotum", "ubiquitous", "obtuse", "spasmodic"};
     private static String[] stringNoun = {"deny", "profound", "scan", "round", "employ", "measure", "chauvinist", "lodge", "restaurant", "preach"};
