448e5b0935cba83fdb31b6746d7e43cf67571670
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-09 22:18:04 -0500

    Logged and tested Dog class.

--- src/PersonalPractice/TestApp.java
+++ src/PersonalPractice/TestApp.java
@@ -4,18 +4,21 @@ public class TestApp {
 
     public static void main(String[] args) {
         Dog d1 = new Dog();
-        d1.size = 40;
-        d1.name = "Milo";
+        System.out.println(d1.size = 40);
+        System.out.println(d1.name = "Milo");
+        System.out.println(d1.breed = "Cattle Dog");
         d1.bark();
 
         Dog d2 = new Dog();
-        d2.size = 60;
-        d2.name = "Mia";
+        System.out.println(d2.size = 60);
+        System.out.println(d2.name = "Mia");
+        System.out.println(d2.breed = "Lab");
         d2.bark();
 
         Dog d3 = new Dog();
-        d3.size = 58;
-        d3.name = "Lucy";
+        System.out.println(d3.size = 58);
+        System.out.println(d3.name = "Lucy");
+        System.out.println(d3.breed = "Arctic fox");
         d3.bark();
 
 
