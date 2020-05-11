4061a97d460af020ef5791c689e63cbcb10daaf2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-11 13:47:35 -0500

    Created new variable Rectangle and assigned it new instance of Square object and called getPerimeter and getArea.

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -6,6 +6,11 @@ public class ShapesTest {
         Rectangle box1 = new Rectangle(4, 5);
         System.out.println(box1.getPerimeter());
         System.out.println(box1.getArea());
+
+        Rectangle box2 = new Square(5);
+        System.out.println(box2.getPerimeter());
+        System.out.println(box2.getArea());
+
     }
 
 }
