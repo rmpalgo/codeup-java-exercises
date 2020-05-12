0a0864e50213ff8feb91a1b2bc69adc2be15c217
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-12 12:14:35 -0500

    Creating the structure for square.

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,6 +1,34 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable {
+
+    public Rectangle(int aLength, int aWidth) {
+        super(aLength, aWidth);
+    }
+
+
+    @Override
+    void setLength(int aLength) {
+        System.out.println("setting length from the Rectangle Class: ");
+
+    }
+
+    @Override
+    void setWidth(int aWidth) {
+        System.out.println("setting width from the Rectangle Class: ");
+    }
+
+    @Override
+    public double getPerimeter() {
+        System.out.println("getting Perimeter from the Rectangle Class: ");
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        System.out.println("getting Area from the Rectangle Class: ");
+        return 0;
+    }
 //    protected int length;
 //    protected int width;
 //
