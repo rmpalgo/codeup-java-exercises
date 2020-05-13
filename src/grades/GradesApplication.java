a383d7c10a9c124ae5e797cd917cff0dc0f93a87
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-13 15:51:01 -0500

    Added persons and 3 grades to each pereson.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,6 +6,20 @@ public class GradesApplication {
 
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
+        Student  shaggy = new Student("Shaggy");
+        shaggy.addGrade(60);
+        shaggy.addGrade(67);
+        shaggy.addGrade(75);
+
+        Student fred = new Student("Fred");
+        fred.addGrade(79);
+        fred.addGrade(85);
+        fred.addGrade(87);
+
+        Student velma = new Student("Velma");
+        velma.addGrade(96);
+        velma.addGrade(99);
+        velma.addGrade(100);
     }
 
 }
