75f0c04895e15c23f51ead8e5f2a4a8f045decf3
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-16 11:22:14 -0500

    Feat: created a directory using method createDir which consists of Files.createDirectory().

--- src/PersonalPractice/FilePractice.java
+++ src/PersonalPractice/FilePractice.java
@@ -1,20 +1,35 @@
 package PersonalPractice;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Formatter;
 
 public class FilePractice {
 
     public static void main(String[] args) {
 
-        File x = new File("src/PersonalPractice/TestFile.txt");
+    String directory = "src/PersonalPractice/data";
+    String filename = "info.txt";
 
-        if(x.exists()) {
-            System.out.println(x.getName() + " exists!");
-        } else {
-            System.out.println("This doesn't exist!");
+    Path dataDirectory = Paths.get(directory);
+    System.out.println("dataDirectory = " + dataDirectory);
+    createDir(dataDirectory);
+
+    }
+
+    public static void createDir(Path aDir) {
+        if(Files.notExists(aDir)) {
+            try {
+                Files.createDirectory(aDir);
+                System.out.println("You just created a file.");
+            } catch (IOException e) {
+                System.out.println("There was an error.");
+                e.printStackTrace();
+            }
         }
-
-
     }
 
 }
